2512770a80b0e855cf23a9d43fd007a1794c324e
deathnotelol <sawnaylinn2017@gmail.com>
2025-02-14 04:39:59 +0000

    date

--- public/js/plugins-init/pickadate-init.js
+++ public/js/plugins-init/pickadate-init.js
@@ -1,7 +1,23 @@
+// (function($) {
+//     "use strict"
+
+//     //date picker classic default
+//     $('.datepicker-default').pickadate();
+
+// })(jQuery);
+
 (function($) {
     "use strict"
 
-    //date picker classic default
-    $('.datepicker-default').pickadate();
+    // Date picker with month and year selection
+    $('.datepicker-default').pickadate({
+        selectMonths: true,  // Enables month selection when clicking the month name
+        selectYears: 20,     // Enables a year dropdown with 20 years range
+        format: 'yyyy-mm-dd', // Adjust the format as needed
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Close',
+        closeOnSelect: false // Keep picker open until user clicks close
+    });
 
 })(jQuery);
